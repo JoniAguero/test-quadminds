99d88a8e6050a699e1c4edbcf380d282e7b2e1bd
Joni Agüero <aguerojonathanivan@gmail.com>
2019-03-15 05:36:05 -0300

    clean

--- client/src/reducers/notes_reducer.js
+++ client/src/reducers/notes_reducer.js
@@ -1,5 +1,5 @@
 import {
-    SET_NOTES, NEW_NOTE, SET_NOTE_BY_ID, CLEAR_NOTE_DETAIL
+    SET_NOTES, NEW_NOTE, SET_NOTE_BY_ID, CLEAR_NOTE_DETAIL, REMOVE_NOTE_BY_ID
 } from '../actions/types';
  
 
@@ -10,10 +10,12 @@ export default function(state={}, action){
             return {...state, notes: action.payload }
         case SET_NOTE_BY_ID:
             return {...state, notes: action.payload }
+        case REMOVE_NOTE_BY_ID:
+            return {...state, notes: action.payload }
         case NEW_NOTE:
             return {...state, notes: action.payload }
         case CLEAR_NOTE_DETAIL:
-            return {...state, prodDetail: action.payload }
+            return {...state, notes: action.payload }
         default:
             return state;
     }
